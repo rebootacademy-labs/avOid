1177c1a5b2016d2f7fba7491e4430247e6a17982
root <root@LAPTOP-D4I8D0R7.localdomain>
2020-02-24 11:06:22 +0000

    Move pointer

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
+// SECTION HTML
+
 const canvas = document.getElementById("canvas");
 var canvasPosition = canvas.getBoundingClientRect() // Nos indica la posición de top,left,rigth y bottom del canvas
 const dot = document.getElementById("personaje");
 var dotTop = 275; // Situamos al personaje en el eje Y
 var dotLeft = 475; // Situamos al personaje en el eje X
 
-/*
-Al empezar el personaje no debería verse: display:none o visibility: hidden en css
-Cuando pulsas el botón start aparece:
-const buttonStart = document.getElementById("id");
-buttonStart.addEventListener("click", function (e){
-    --//Cambiar estado personaje en Css
-})
-*/
+//Al empezar el personaje no debería verse: display:none o visibility: hidden en css
+//Cuando pulsas el botón start aparece:
+//const buttonStart = document.getElementById("id");
+//buttonStart.addEventListener("click", function (e){
+//    --//Cambiar estado personaje en Css
+//})
 
 // MOVIMIENTO DEL PERSONAJE:
 
@@ -23,18 +23,16 @@ canvas.addEventListener("mousemove", function (e) {
     document.getElementById("demo").innerHTML = coor;
 
 // 2. Relacionar la posición del dot con puntero
-
     dot.style.left = mousePositionX - 8 + "px";
     dot.style.top = mousePositionY - 8 + "px";
 
 // 3. Alerta LOSE
-   /* if (e.clientX <= canvasPosition.left || e.clientX >= canvasPosition.right) {
-        alert("U LOSE");
+    if (e.clientX <= canvasPosition.left || e.clientX >= canvasPosition.right) {
+        console.log("U LOSE");
     }
     if (e.clientY <= canvasPosition.top || e.clientY >= canvasPosition.bottom) {
-        alert("U LOSE");
+        console.log("U LOSE");
     }
-    */
    //.addEventListener("mouseleave", restartGame (e) )
 })
 
@@ -42,12 +40,9 @@ function restartGame() {
     alert("U LOSE");
 }
 
-
-/*
-Necesitamos el boton START para establecer un setInterval para los niveles  y otro para la vida
-const buttonStart = document.getElementById("id");
-buttonStart.addEventListener("click", function (e){
-    --//timerId = setInterval(function(){}, time)
-})
-clearInterval(timerId) when LOSE;
-*/
+//Necesitamos el boton START para establecer un setInterval para los niveles  y otro para la vida
+//const buttonStart = document.getElementById("id");
+//buttonStart.addEventListener("click", function (e){
+//    --//timerId = setInterval(function(){}, time)
+//})
+//clearInterval(timerId) when LOSE;
