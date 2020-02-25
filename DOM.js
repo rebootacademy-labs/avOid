106bd6c4611b39ea8c913aea64fc575d0b7aca51
Adrian <adrivelazquez97@gmail.com>
2020-02-25 15:03:45 +0000

    Finished refactor

--- DOM.js
+++ DOM.js
@@ -1,18 +1,5 @@
-const canvas = document.getElementById("canvas");
-var canvasPosition = canvas.getBoundingClientRect() // Nos indica la posición de top,left,rigth y bottom del canvas
-const dot = document.getElementById("personaje");
-const enemy = document.querySelectorAll(".enemy");
-var dotTop; // Situamos al personaje en el eje Y
-var dotLeft; // Situamos al personaje en el eje X
-var dotBottom;
-var dotRight;
 var totalSeconds= 0;
 var actualLevel = 1;
-
-var enemyTop = 100;
-var enemyLeft = 600;
-var enemyBottom = 118;
-var enemyRight = 618;
 
 function setLevel() {
   actualLevel++;
@@ -22,7 +9,6 @@ var timerLevel = setInterval(() => {
   setLevel();
 }, 20000)
 
-
 function setTime() {
   totalSeconds++;
   time.innerHTML = totalSeconds + "s";
@@ -31,28 +17,15 @@ var timerSeconds = setInterval(() => {
   setTime();
 }, 1000);
 
-/*
-function Enemy (enemyTop, enemyLeft, width, height, color){
-  this.enemyTop = enemyTop;
-  this.enemyLeft = enemyLeft;
-  this.width = width;
-  this.height = height;
-  this.color = color,
-  this.create = function(){};
-  this.move = function(){};
-}
+const canvas = document.getElementById("canvas");
+const dot = document.getElementById("personaje");
 
-*/
-
-
-var newDot = new MainDot()
-
+var newDot = new MainDot(canvas, dot)
 newDot.move()
 
 var randomEnemies = setInterval(() => {
-  var enemyDot = new Enemy()
-  console.log(enemyDot);
-  
+  var enemyDot = new Enemy()  
   enemyDot.move()
 }, 100);
+
 
--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-function MainDot () {
-  this.dot = document.getElementById("personaje");  
-  this.canvas = document.getElementById("canvas");
-  this.canvasPosition = canvas.getBoundingClientRect();
+function MainDot (canvas, dot) {
+  this.dot = dot;
+  this.canvas = canvas;
+  this.canvasPosition = this.canvas.getBoundingClientRect();
   
   this.move = function() {
     var that = this
     canvas.addEventListener("mousemove", function (e) {
-      var canvasMouseX = e.clientX - (canvasPosition.left + 12);
-      var canvasMouseY = e.clientY - (canvasPosition.top + 12);      
+      var canvasMouseX = e.clientX - (that.canvasPosition.left + 12);
+      var canvasMouseY = e.clientY - (that.canvasPosition.top + 12);      
 
       var dotLeft = canvasMouseX - that.dot.offsetWidth / 2;
       var dotTop = canvasMouseY - that.dot.offsetHeight / 2;
       var dotRight = dotLeft + that.dot.offsetWidth;
       var dotBottom = dotTop + that.dot.offsetHeight;
+
+      //collision()
 
       that.dot.style.left = dotLeft + "px";
       that.dot.style.top = dotTop + "px";
@@ -25,19 +27,19 @@ function MainDot () {
       if (dotTop <= 0 || dotBottom >= 600) {
         console.log("U LOSE");
       }
-
-      /*
-      var sumRadios = that.dot.offsetWidth / 2 + enemy[0].offsetWidth / 2;
-      var x = (dotLeft + (that.dot.offsetWidth / 2)) - (enemyLeft + (enemy[0].offsetWidth / 2));
-      var y = (dotTop + (that.dot.offsetHeight / 2)) - (enemyTop + (enemy[0].offsetHeight / 2));
+    })
+  }
+/*
+  this.collision = function(enemyArray) {
+      var sumRadios = this.dot.offsetWidth / 2 + enemy[0].offsetWidth / 2;
+      var x = (dotLeft + (this.dot.offsetWidth / 2)) - (enemyLeft + (enemy[0].offsetWidth / 2));
+      var y = (dotTop + (this.dot.offsetHeight / 2)) - (enemyTop + (enemy[0].offsetHeight / 2));
     
       if (sumRadios > Math.sqrt((x * x) + (y * y))) {
         console.log("BOOM!");
       }
-
-      */
-    })
   }
+  */
 }
 
 function Enemy (speed) {
@@ -54,7 +56,6 @@ function Enemy (speed) {
     var that = this
     let movement = setInterval(function () {
       that.newEnemy.style.top = parseInt(that.newEnemy.style.top.slice(0, that.newEnemy.style.top.length - 2)) + 1 + "px";
-      //that.newEnemy.style.left += -1 + "px";
       if (that.newEnemy.style.top == "590px") {
         clearInterval(movement);
         that.newEnemy.remove();
