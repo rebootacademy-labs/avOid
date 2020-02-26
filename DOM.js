bfef4120e78709d5ddced1b289443fa8d301a92f
root <root@LAPTOP-D4I8D0R7.localdomain>
2020-02-26 01:58:37 +0000

    WIP START

--- DOM.js
+++ DOM.js
@@ -1,31 +1,54 @@
+var startButton = document.getElementById("start");
+var startBackground = document.getElementById("menu");
 var totalSeconds= 0;
 var actualLevel = 1;
-var levelTime = 1;
-function setLevel() {
-  actualLevel++;
-  level.innerHTML = actualLevel;
-}
-var timerLevel = setInterval(() => {
-  setLevel();
-}, 20000)
 
-function setTime() {
-  totalSeconds++;
-  time.innerHTML = totalSeconds + "s";
+function initiate () {
+  startButton.addEventListener("click", function(actualLevel, totalSeconds){
+    startBackground.classList.add ("desactivate");
+    startButton.classList.add ("desactivate");
+
+    dot.classList.remove ("desactivate");
+    wrapper.classList.remove("desactivate");
+    timerInfo.classList.remove("desactivate");
+
+    function setLevel() {
+      actualLevel++;
+      level.innerHTML = actualLevel;
+    }
+    var timerLevel = setInterval(() => {
+      setLevel();
+    }, 20000)
+    
+    function setTime() {
+      totalSeconds++;
+      time.innerHTML = totalSeconds + "s";
+    }
+    var timerSeconds = setInterval(() => {
+      setTime();
+    }, 1000);
+  })
 }
-var timerSeconds = setInterval(() => {
-  setTime();
-}, 1000);
 
 const canvas = document.getElementById("canvas");
 const dot = document.getElementById("personaje");
+var wrapper = document.getElementById("wrapper-enemy");
+var timerInfo = document.getElementById("timer");
+
 
 var newDot = new MainDot(canvas, dot)
 newDot.move()
 
-var randomEnemies = setInterval(() => {
-  var enemyDot = new Enemy()
-  enemyDot.move(newDot)
-}, 100);
+var randomEnemiesTop = setInterval(() => {
+  var enemyDotTop = new EnemyTop()
+  enemyDotTop.move(newDot)
+}, 280);
+
+var randomEnemiesRight = setInterval(() => {
+  var enemyDotRight = new EnemyRight()
+  enemyDotRight.move(newDot)
+}, 280);
+
+
 
 
